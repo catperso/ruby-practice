8878feec8d39b18014679cedff57fa5cd42a68c7
Matt Casperson <mc.casperson@gmail.com>
2021-12-06 13:42:57 -0800

    add second test to leetspeek_spec.rb and refactor leetspeek.rb to pass it
    Co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

--- lib/leetspeek.rb
+++ lib/leetspeek.rb
@@ -1,5 +1,16 @@
+require('pry')
+
 String.prepend(Module.new do
   def leetspeek
-    self
+    split_array = self.split('')
+    output_array = []
+    split_array.each do |letter|
+      if letter.upcase.eql?('E')
+        output_array.push('3')
+      else
+        output_array.push(letter)
+      end
+    end
+    output_array.join
   end
 end)
--- spec/leetspeek_spec.rb
+++ spec/leetspeek_spec.rb
@@ -5,4 +5,8 @@ describe('String#leetspeek') do
   it('returns a string as is when no leetspeek rules apply') do
     expect("crypt".leetspeek).to(eq("crypt"))
   end
+
+  it('returns every "e" in a string with a "3"') do
+    expect("egg".leetspeek).to(eq("3gg"))
+  end
 end
